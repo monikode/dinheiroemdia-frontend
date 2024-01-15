d140c3b1dfa8156d82a5fbf770c00f0dfd4bd8ff
Monike <monike.ftsousa@gmail.com>
2024-01-15 15:21:03 -0400

    api create user

---
+++ src/pages/protected/accounts/list/index.tsx
@@ -0,0 +1,16 @@
+import { Grid, Typography } from "@mui/material";
+import { CardTable } from "../../../../shared/components/card-table/index";
+
+export function Accounts() {
+  return (
+    <Grid container direction={"column"} flexWrap={"nowrap"}>
+        <Grid item xs={6}>
+          <Typography variant="h4">Categorias</Typography>
+        </Grid>
+
+        <Grid item xs={6}>
+          <CardTable></CardTable>
+        </Grid>
+    </Grid>
+  );
+}
