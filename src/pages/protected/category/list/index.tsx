70b885a1156809d1f7ec1ff27d9f42284b6d9a8a
Monike <monike.ftsousa@gmail.com>
2024-01-17 15:44:11 -0400

    dialog form

--- src/pages/protected/category/list/index.tsx
+++ src/pages/protected/category/list/index.tsx
@@ -1,16 +1,37 @@
 import { Grid, Typography } from "@mui/material";
-import { CardTable } from "../../../../shared/components/card-table/index";
+import { useState } from "react";
+import { Button } from "../../../../../node_modules/@mui/material/index";
+import { CardItemProps, CardTable } from "../../../../shared/components/card-table/index";
+import StyledDialog from "../../../../shared/components/dialog/index";
+import { CategoryForm } from "../form/index";
 
 export function Categories() {
+  const [list, setList] = useState<CardItemProps[]>(Array(10).fill({
+    id: 1,
+    name: "Alimentação",
+    consumption: 100.00,
+    percentage: 5,
+
+  }))
+  const [openDialog, setOpenDialog] = useState(true)
+  const onCreate = () => {
+    setOpenDialog(true)
+  }
+
+  const onClose = () => {
+    setOpenDialog(false)
+  }
   return (
     <Grid container direction={"column"} flexWrap={"nowrap"}>
-        <Grid item xs={6}>
-          <Typography variant="h4">Categorias</Typography>
-        </Grid>
+      <Grid item xs={6}>
+        <Typography variant="h4">Categorias</Typography>
+      </Grid>
 
-        <Grid item xs={6}>
-          <CardTable></CardTable>
-        </Grid>
+      <Grid item xs={6}>
+        <CardTable onCreate={onCreate} list={list} itemRoute="/categoria/"></CardTable>
+      </Grid>
+      <StyledDialog openProps={openDialog} onClose={onClose} onConfirm={onClose} title="Nova Categoria">
+        <CategoryForm></CategoryForm></StyledDialog>
     </Grid>
   );
 }
